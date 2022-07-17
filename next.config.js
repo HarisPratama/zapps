8a4b179a911031aa7c4b2ec2014cbbd090e71573
HarisPratama <hpratama324@gmail.com>
2022-07-17 17:53:07 +0700

    update

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
 	swcMinify: true,
 
 	env: {
+		NEXTAUTH_URL: 'http://localhost:3000',
+		NEXTAUTH_SECRET: '521f898d2b89180f34663a724fc70f24',
 		google: {
 			client_id: '736616171846-ma8krke9gbfm16tdpmg7a17j27o14h13.apps.googleusercontent.com',
 			client_secret: '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'
